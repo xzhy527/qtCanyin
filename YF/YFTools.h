8245d7286f4df5a815d5de8c1e583676a049bb4a
YiFeng <xzhy527@126.com>
2014-11-13 10:46:44 +0800

    554555

---
+++ YF/YFTools.h
@@ -0,0 +1,12 @@
+#ifndef YFTOOLS_H
+#define YFTOOLS_H
+#include <QMessageBox>
+#include <QApplication>
+
+
+void YF_alert(QString text)
+{
+    QMessageBox::about(qApp->activeWindow(),"提示",text);
+}
+
+#endif // YFTOOLS_H
