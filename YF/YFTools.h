564434182e54873cc9fffe140984e2f983f07e64
YiFeng <xzhy527@126.com>
2014-11-13 10:48:52 +0800

    ddd

--- YF/YFTools.h
+++ YF/YFTools.h
@@ -2,11 +2,8 @@
 #define YFTOOLS_H
 #include <QMessageBox>
 #include <QApplication>
-
-
 void YF_alert(QString text)
 {
     QMessageBox::about(qApp->activeWindow(),"提示",text);
 }
-
 #endif // YFTOOLS_H
